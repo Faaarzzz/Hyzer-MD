c8c58d74105c72c64531c0387503b72a7e376de0
Gaara Setyawan <90649022+GaaraDel@users.noreply.github.com>
2022-06-14 13:14:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 //=======『 Tampilan Dan Lainnya 』============//
 global.fotonya1 = 'https://telegra.ph/file/62067cdec8344279fc042.jpg' //ganti jadi foto bot mu
 global.fotonya2 = 'https://telegra.ph/file/d6048fda84f0223936c92.jpg' //ini juga ganti 
-global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
+global.lolkey = 'Memek11' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = '𝙶𝚊𝚊𝚛𝚊 𝙱𝚘𝚝'
 global.watermark = wm
@@ -36,8 +36,8 @@ global.eror = '_*Server Emrror*_'
 global.benar = 'Benar ✅\n'
 global.salah = 'Samlah ❌\n'
 global.stiker_wait = 'Stiker semdang dibuat'
-global.packname = '2022'
-global.author = '© 𝙶𝚊𝚊𝚛𝚊 𝙱𝚘𝚝'
+global.packname = 'stikerby 𝙶𝚊𝚊𝚛𝚊 𝙱𝚘𝚝'
+global.author = '@gaara_xploite'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
@@ -77,7 +77,7 @@ global.APIKeys = { // APIKey Here
   'https://pencarikode.xyz': 'pais', 
   'https://leyscoders-api.herokuapp.com': 'dappakntlll',
   'https://zekais-api.herokuapp.com': 'apikeymu',
-  'https://api.lolhuman.xyz': 'Deffbotz',
+  'https://api.lolhuman.xyz': 'Memek11',
 }
 
 //=============『 RPG GAMES 』================== //
